7cc92b80fb2a1129e1a72bd05b65db8775d31979
Omid Sharafzadeh <omid.sharafzadeh@topcashback.co.uk>
2017-01-25 12:42:30 +0000

    yru

---
+++ RefactoringTechniques/LongMethod/BasketManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace LongMethod
+{
+    public class BasketManager
+    {
+        private const decimal MaxCubicVolumeForProduct = 156m;
+
+        public void AddToBasket(
+            Product product, 
+            int quantity, 
+            Basket basket, decimal winterRate, decimal summerRate, decimal winterServiceCharge)
+        {
+            var summerStart = DateTime.Now;
+            var summerEnd = DateTime.Now;
+            var now = DateTime.Now;
+            var basePrice = product.Price * quantity;
+
+            decimal totalPrice;
+            decimal charge;
+
+            if (now < summerStart || now > summerEnd) //if winter then charge winter rate
+            {
+                charge = quantity * winterRate + winterServiceCharge;
+                totalPrice = charge + basePrice;
+            }
+            else //else charge summer rate
+            {
+                charge = quantity * summerRate;
+                totalPrice = charge + basePrice;
+            }
+
+            basket.Add(product);
+            basket.TotalPrice += totalPrice;
+        }
+
+        public bool FitsIntoTheBox(decimal width, decimal height, decimal depth, decimal cubicArea)
+        {
+            return (width * height * depth) <= cubicArea;
+        }
+
+        public bool IsAboveMaxCubicVolume(decimal width, decimal height, decimal depth)
+        {
+            return (width * height * depth) >MaxCubicVolumeForProduct;
+        }
+    }
+    public class Product
+    {
+        public decimal Price { get; set; }
+    }
+
+    public class Basket
+    {
+        public decimal TotalPrice { get; set; }
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RefactoringTechniques/LongMethod/BasketManagerRefactored.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace LongMethod
+{
+    public class Measurement
+    {
+        public Measurement(decimal width, decimal height, decimal depth)
+        {
+            Width = width;
+            Height = height;
+            Depth = depth;
+        }
+
+        public decimal Width { get; }
+        public decimal Height { get; }
+        public decimal Depth { get; }
+
+        public decimal GetCubicVolume()
+        {
+            return Width * Height * Depth;
+        }
+    }
+
+    public class BasketManagerRefactored
+    {
+        private const decimal MaxCubicVolumeForProduct = 156m;
+
+        private int _quantity;
+        private Product _product;
+        private decimal _winterRate;
+        private decimal _summerRate;
+        private decimal _winterServiceCharge;
+
+        public Basket AddToBasket(Product product, int quantity, Basket basket, decimal winterRate, decimal summerRate, decimal winterServiceCharge)
+        {
+            //These fields can also be initialisde from the constructor if you want to keep the Manager class tied to a single product
+            _winterServiceCharge = winterServiceCharge;
+            _summerRate = summerRate;
+            _winterRate = winterRate;
+            _product = product;
+            _quantity = quantity;
+
+            var totalPrice = GetTotalPriceForSeason();
+
+            basket.Add(product);
+            basket.TotalPrice += totalPrice;
+
+            return basket;
+
+        }
+
+        private decimal GetTotalPriceForSeason()
+        {
+            if (NotSummer()) //if winter then charge winter rate
+            {
+                return GetTotalPrice(WinterCharge());
+            }
+
+            return GetTotalPrice(SummerCharge());
+        }
+
+        private decimal SummerCharge()
+        {
+            return _quantity * _summerRate;
+        }
+
+        private decimal GetTotalPrice(decimal charge)
+        {
+            return charge + GetBasePrice();
+        }
+
+        private decimal GetBasePrice()
+        {
+            return _product.Price * _quantity;
+        }
+
+        private decimal WinterCharge()
+        {
+            return _quantity * _winterRate + _winterServiceCharge;
+        }
+
+        private static bool NotSummer()
+        {
+            var summerStart = DateTime.Now;
+            var summerEnd = DateTime.Now;
+            var now = DateTime.Now;
+            return now < summerStart || now > summerEnd;
+        }
+
+        public bool FitsIntoTheBox(Measurement measurement, decimal cubicArea)
+        {
+            return (measurement.GetCubicVolume()) <= cubicArea;
+        }
+
+        public bool IsAboveMaxCubicVolume(Measurement measurement)
+        {
+            return (measurement.GetCubicVolume()) > MaxCubicVolumeForProduct;
+        }
+    }
+}
